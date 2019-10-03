618b4483f5ca0e7c7f2f8e7053b0c14ac23f051c
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-02 21:44:18 -0700

    print the desired output by making the object

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -6,9 +6,11 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String a = obj.nextLine().trim();
-        System.out.println("index:\t");
+        System.out.print("index:\t");
         Output obj1 = new Output(a);
-
+        System.out.println(obj1.index());
+        System.out.print("chars:\t");
+        System.out.println(obj1.chara());
 
     }
 }
