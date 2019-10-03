42c1a8b1fbecd36f3d9459d5383d76d7adddf009
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-02 21:26:32 -0700

    taking input from the user by using the object of the scanner class

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        String a = obj.nextLine().trim();
     }
 }
