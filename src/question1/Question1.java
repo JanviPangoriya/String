051c6e323883485a0485f495173922e67ef58b9a
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-02 21:42:47 -0700

    step to print the desired output

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -6,6 +6,7 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String a = obj.nextLine().trim();
+        System.out.println("index:\t");
 
     }
 }
