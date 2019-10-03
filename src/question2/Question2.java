70c724fd08c749aaef3988577a4cee0555005d02
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-02 21:55:12 -0700

    taking input from the user for String and integer variable

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        String as = obj.nextLine().trim();
+        int index = obj.nextInt();
+
     }
 }
