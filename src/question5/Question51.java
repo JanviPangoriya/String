438e31faa6aad02668876e453d938ec2657caf2d
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 22:34:20 -0700

    making getter and setter

--- src/question5/Question51.java
+++ src/question5/Question51.java
@@ -10,4 +10,28 @@ public class Question51 {
         this.string = string;
         this.substring = substring;
     }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String getReplacestring() {
+        return replacestring;
+    }
+
+    public void setReplacestring(String replacestring) {
+        this.replacestring = replacestring;
+    }
+
+    public String getSubstring() {
+        return substring;
+    }
+
+    public void setSubstring(String substring) {
+        this.substring = substring;
+    }
 }
