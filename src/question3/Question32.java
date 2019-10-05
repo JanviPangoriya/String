4652876d5b77250639411e0cc2006bd43ade700b
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 19:48:04 -0700

    change the name of class to Question32 and make pararmeterized character

---
+++ src/question3/Question32.java
@@ -0,0 +1,10 @@
+package question3;
+
+public class Question32 {
+    private String input;
+    private String vowel = "AEIOUaeiou";
+
+    Question32(String input) {
+        this.input = input;
+    }
+}
--- src/question3/Qustion32.java
+++
@@ -1,6 +0,0 @@
-package question3;
-
-public class Qustion32 {
-    private String input;
-    private String vowel = "AEIOUaeiou";
-}
