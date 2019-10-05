abe19697907ef3db6aca722fdadbc478ae84f904
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 22:32:50 -0700

    making parameterized constructor

--- src/question5/Question51.java
+++ src/question5/Question51.java
@@ -4,4 +4,10 @@ public class Question51 {
     protected String string;
     protected String substring;
     protected String replacestring;
+
+    Question51(String string, String substring, String replacestring) {
+        this.replacestring = replacestring;
+        this.string = string;
+        this.substring = substring;
+    }
 }
