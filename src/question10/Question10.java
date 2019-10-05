016445f6bc6eff43438797a763935a4a4a75c4e9
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 21:26:36 -0700

    take string as a input from the user

--- src/question10/Question10.java
+++ src/question10/Question10.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class Question10 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        String string = obj.next().trim();
     }
 }
