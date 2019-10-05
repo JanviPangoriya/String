d7b8c2c25dee8d7ef6592bbd2615b5f76ad44f81
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 22:12:46 -0700

    write logic for the desired output

--- src/question4/Question42.java
+++ src/question4/Question42.java
@@ -3,9 +3,11 @@ package question4;
 public class Question42 {
     protected String s1 = "";
     protected String s2 = "";
+    protected String s3 = "";
 
     Question42(String input) {
         s2 = input.toLowerCase();
+        s3 = input;
     }
 
     public String getS1() {
@@ -24,4 +26,24 @@ public class Question42 {
         this.s2 = s2;
     }
 
+    public void output() {
+        int counter = 0;
+        for (int i = 0; i < s2.length(); i++) {
+            if (s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i' || s2.charAt(i) == 'o' || s2.charAt(i) == 'u') {
+                counter += 1;
+
+                if (counter % 3 == 1) {
+                    s1 += "*";
+                } else if (counter % 3 == 2) {
+                    s1 += "^";
+                } else {
+                    s1 += "!";
+                }
+            } else {
+                s1 += s3.charAt(i);
+            }
+
+        }
+        System.out.println(s1);
+    }
 }
