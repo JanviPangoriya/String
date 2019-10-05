d26121ee0027994227d4b2df4b779a86b877779f
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 20:40:33 -0700

    making object of defination class

--- src/question8/Question81.java
+++ src/question8/Question81.java
@@ -6,5 +6,6 @@ public class Question81 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String string = obj.next().trim();
+        Question82 obj1 = new Question82(string)
     }
 }
