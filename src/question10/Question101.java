4bc0a90ca788d0cc7e98c8ae4a850e820f43f82c
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 21:38:25 -0700

    making getter and setter

--- src/question10/Question101.java
+++ src/question10/Question101.java
@@ -4,4 +4,19 @@ public class Question101 {
     protected String string;
     protected char c;
 
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public void setC(char c) {
+        this.c = c;
+    }
 }
