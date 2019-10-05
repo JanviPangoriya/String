7ae73f85c0ca0b1c593d63990e3bcc115c88e398
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 19:43:31 -0700

    making object of defination class for accessing its method and then print the desired output by using output method.

--- src/question3/Question31.java
+++ src/question3/Question31.java
@@ -6,6 +6,8 @@ public class Question31 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String input = obj.next().trim();
+        Question32 obj1 = Question32(input);
+        System.out.println(obj.output());
 
     }
 }
