c14f99f3ad250468979e9a281c229707373577a1
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 22:24:13 -0700

    taking string substring and replace string fro the user

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Question5 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-
+        String string = obj.nextLine().trim();
+        String substring = obj.nextLine().trim();
+        String replacestring = obj.nextLine().trim();
     }
 }
