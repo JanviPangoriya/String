12eda00a95b9924c2c7eac34a8a7fb5cd07084a6
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 21:52:06 -0700

    making parameterized constructor

---
+++ src/question11/Question112.java
@@ -0,0 +1,12 @@
+package question11;
+
+public class Question112 {
+    protected String string;
+    protected char c;
+
+    Question112(String string, char c) {
+        this.string = string;
+        this.c = c;
+    }
+
+}
