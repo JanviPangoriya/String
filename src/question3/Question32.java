00ec176b74b877f86a551e5caf7fed129f5e825a
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 19:52:58 -0700

    write the logic to return the desired output.

--- src/question3/Question32.java
+++ src/question3/Question32.java
@@ -15,4 +15,17 @@ public class Question32 {
     public void setInput(String input) {
         this.input = input;
     }
+
+    public String output() {
+        for (int i = 0; i < vowel.length(); i++) {
+            for (int j = 0; j < input.length(); j++) {
+                if (vowel.charAt(i) == input.length(j)) {
+                    input = input.replace(input.charAt(j), '*');
+                }
+
+            }
+
+        }
+        return input;
+    }
 }
