046fecf2073dfc18c75cdd1f403d561295c00473
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 19:48:47 -0700

    making getter and setter

--- src/question3/Question32.java
+++ src/question3/Question32.java
@@ -7,4 +7,12 @@ public class Question32 {
     Question32(String input) {
         this.input = input;
     }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
 }
