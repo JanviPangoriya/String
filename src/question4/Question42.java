819b04b986609849af441f45cd6e28d5ea1f8e21
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 22:03:50 -0700

    making getter and setter

--- src/question4/Question42.java
+++ src/question4/Question42.java
@@ -8,4 +8,20 @@ public class Question42 {
         s2 = input.toLowerCase();
     }
 
+    public String getS1() {
+        return s1;
+    }
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+
+    public String getS2() {
+        return s2;
+    }
+
+    public void setS2(String s2) {
+        this.s2 = s2;
+    }
+
 }
