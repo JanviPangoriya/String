2a6f74ee5bc71d50867e0d939589cf85819a8e9e
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 21:47:00 -0700

    take two input from the user one is string another is character

--- src/question11/Question111.java
+++ src/question11/Question111.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Question111 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        String string = obj.next().trim();
+        char c = obj.next().trim().charAt(0);
+
     }
 }
