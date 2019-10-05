3bd47a0663f3aa92cb3a289637045e6ee384a3b0
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 20:08:14 -0700

    import scanner class for taking input from the user and making its object.

---
+++ src/question4/Question41.java
@@ -0,0 +1,7 @@
+package question4;
+
+public class Question41 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/question5/Question52.java
@@ -0,0 +1,9 @@
+package question5;
+
+import java.util.Scanner;
+
+public class Question52 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+    }
+}
