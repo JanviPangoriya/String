f5de19f3f981a4153c19b4167def09413c153e5f
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 19:55:06 -0700

    modify the object

--- src/question3/Question31.java
+++ src/question3/Question31.java
@@ -6,8 +6,8 @@ public class Question31 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String input = obj.next().trim();
-        Question32 obj1 = Question32(input);
-        System.out.println(obj.output());
+        Question32 obj1 = new Question32(input);
+        System.out.println(obj1.output());
 
     }
 }
--- src/question3/Question32.java
+++ src/question3/Question32.java
@@ -29,3 +29,4 @@ public class Question32 {
         return input;
     }
 }
+
