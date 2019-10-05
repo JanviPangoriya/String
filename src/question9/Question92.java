265d2cd689c82f33322104e24e46c3b05f789fd6
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 21:17:22 -0700

    making getter and setter

--- src/question9/Question92.java
+++ src/question9/Question92.java
@@ -6,4 +6,12 @@ public class Question92 {
     Question92(String string) {
         this.string = string;
     }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
 }
