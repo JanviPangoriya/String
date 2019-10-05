f45583b1fdad754f15ea4bfe8fe08087e638ac8c
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 22:15:23 -0700

    taking string input from the user and make object of defination class and then call the output method to print the desired output

--- src/question4/Question41.java
+++ src/question4/Question41.java
@@ -1,7 +1,12 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question41 {
     public static void main(String[] args) {
-
+        Scanner obj = new Scanner(System.in);
+        String input = obj.next().trim();
+        Question42 obj1 = new Question42(input);
+        obj1.output();
     }
 }
