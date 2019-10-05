c9bfe8093339d09ff026c17ce5c100f7b6a44cd4
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-10-04 20:26:43 -0700

    making object of defination class and then print the desired output

--- src/question7/Question71.java
+++ src/question7/Question71.java
@@ -6,5 +6,7 @@ public class Question71 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String input = obj.next().trim();
+        Question72 obj1 = new Question72(input);
+        System.out.println(obj.output());
     }
 }
